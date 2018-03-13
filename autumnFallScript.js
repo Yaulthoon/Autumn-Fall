611056f9424680b47c601a24032c3c75ffaa041b
Brandon <anodyne4@hotmail.com>
2018-03-12 21:55:37 -0400

    Add files via upload

---
+++ autumnFallScript.js
@@ -0,0 +1,25 @@
+function dropdown1() {
+    document.getElementById("content1").classList.toggle("show");
+}
+
+function dropdown2() {
+    document.getElementById("content2").classList.toggle("show");
+}
+
+function dropdown3() {
+    document.getElementById("content3").classList.toggle("show");
+}
+
+function dropdown4() {
+    document.getElementById("content4").classList.toggle("show");
+}
+
+function dropdown5() {
+    document.getElementById("content5").classList.toggle("show");
+}
+
+function getRandomInteger(min, max) {
+
+return Math.floor(Math.random()*(max - min) +min;
+
+}
